cd64c8dc22274c5c46bd4e5e469699770901740d
krishabh080 <86287145+krishabh080@users.noreply.github.com>
2023-09-07 03:13:49 +0530

    Update ec.py

--- backend/ec.py
+++ backend/ec.py
@@ -4,17 +4,17 @@ import boto3
 print("Content-type:text/html")
 print()
 
-print("fsdfafa")
+
 
 import subprocess as s
-out=s.getoutput("aws ec2 run-instances --image-id ami-0ded8326293d3201b  --instance-type t2.micro")
+out=s.getoutput("aws ec2 run-instances --image-id {enter you ami_id}  --instance-type {enter instance type}")
 print("<pre>")
 print(out)
 print("</pre>")
 
 print()
 print()
-print("<form action= http://3.108.228.1/menu.html>")
+print("<form action= HTTP://'your_ip'/menu.html>")
 print("<input type='submit' value='Back to Main menu'></form>")
 
 
